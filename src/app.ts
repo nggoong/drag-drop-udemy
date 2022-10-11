293add10154aaa538133bf6b20f60853ad1ac806
nggoong <nggoong@naver.com>
2022-10-11 12:46:21 +0900

    FEAT: autobind 데코레이터 생성 및 적용

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,17 @@
 // Code goes here!
-console.log("htllo")
+// auto bind decorator
+function autobind(_:any, _2:string, descriptor:PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor:PropertyDescriptor = {
+        configurable:true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    }
+
+    return adjDescriptor;
+}
 class ProjectInput {
     templateElement:HTMLTemplateElement;
     hostElement:HTMLDivElement;
@@ -25,13 +37,14 @@ class ProjectInput {
         this.attach();
     }
 
+    @autobind
     private submitHandler(event:Event) {
         event.preventDefault();
         console.log(this.titleInputElement.value);
     }
 
     private configure() {
-        this.element.addEventListener('submit', this.submitHandler.bind(this))
+        this.element.addEventListener('submit', this.submitHandler)
     }
 
     private attach() {
