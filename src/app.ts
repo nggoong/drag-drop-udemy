dafcc3f264bd64f8ea784d93982fcca163bd4ea1
nggoong <nggoong@naver.com>
2022-10-12 17:26:02 +0900

    FEAT: 싱글톤으로 어플리케이션 상태 관리하기

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,38 @@
-// Code goes here!
+// Project State Management
+
+class ProjectState {
+    private listeners:any[] = [];
+    private projects: any[] = [];
+    private static instance: ProjectState;
+    private constructor() {
+
+    }
+    static getInstance() {
+        if(this.instance) {
+            return this.instance;
+        }
+        this.instance = new ProjectState();
+        return this.getInstance;
+    }
+
+    addListener(listenerFn:Function) {
+        this.listeners.push(listenerFn);
+    }
+    addProject(title:string, description:string, numOfPeople:number) {
+        const newProject = {
+            id:Math.random().toString(),
+            title:title,
+            description:description,
+            people:numOfPeople
+        };
+        this.projects.push(newProject);
+        for(const listenerFn of this.listeners) {
+            listenerFn(this.projects.slice());
+        }
+    }
+
+}
+const projectState = ProjectState.getInstance();
 
 //validation logic
 interface Validatable {
@@ -47,15 +81,30 @@ class ProjectList {
     templateElement:HTMLTemplateElement;
     hostElement:HTMLDivElement;
     element:HTMLElement;
+    assignedProjects:any[];
 
     constructor(private type:'active' | 'finished') {
         this.templateElement = <HTMLTemplateElement>document.getElementById('project-list')!;
         this.hostElement = <HTMLDivElement>document.getElementById('app')!;
+        this.assignedProjects = [];
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = <HTMLElement>importedNode.firstElementChild;
         this.element.id = `${this.type}-projects`;
+        projectState.addListener((projects:any[])=>{
+            this.assignedProjects = projects;
+            this.renderProjects();
+        })
         this.attach();
         this.renderContent();
+    }
+
+    private renderProjects() {
+        const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+        for(const prjItem of this.assignedProjects) {
+            const listItem = document.createElement("li");
+            listItem.textContent = prjItem.title;
+            listEl?.appendChild(listItem);
+        }
     }
 
     private renderContent() {
